0680c173bb73455a92f75bfec4ab10f989ffbc9b
Joseph Chao <ctzuchiang@gmail.com>
2020-04-23 23:07:53 +0900

    List Sample

--- src/components/ListSample.js
+++ src/components/ListSample.js
@@ -4,18 +4,23 @@ class ListSample extends React.Component {
   constructor(props) {
     super(props);
   }
-
-  NumberList(numbers) {
-    const listItems = numbers.map((number) => (
-      <li key={number}>{number}</li>
-    ));
-    return <ul>{listItems}</ul>;
-  }
-
   render() {
     const numbers = [1, 2, 3, 4, 5];
-    return this.NumberList(numbers);
+    return <NumberList numbers={numbers} />;
   }
+}
+
+function NumberList(props) {
+  const numbers = props.numbers;
+  const listItems = numbers.map((number) => (
+    <ListItem key={number.toString()} value={number} />
+  ));
+  return <ul>{listItems}</ul>;
+}
+
+function ListItem(props) {
+  const value = props.value;
+  return <li>{value}</li>;
 }
 
 export default ListSample;
